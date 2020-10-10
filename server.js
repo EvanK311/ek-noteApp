16e47b9652c74d94c16efcf89f211f16445b36c0
Evan K <EvanK311@gmail.com>
2020-10-09 22:02:25 -0700

    trying fixes

--- server.js
+++ server.js
@@ -15,9 +15,6 @@ app.use(express.static("public"));
 
 // Routes
 // 
-app.get("/", function (req, res) {    
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-})
 
 app.get("/notes", function (req, res) {    
     res.sendFile(path.join(__dirname, "/public/notes.html"));
