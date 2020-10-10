f697bf52a960608b8818436571b7d7b3c1e75e28
Evan K <EvanK311@gmail.com>
2020-10-09 21:36:19 -0700

    heroku problems

--- server.js
+++ server.js
@@ -64,6 +64,10 @@ const pullData = function () {
 
 // Listener and default route
 // 
+app.get("/", function (req, res) {    
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+})
+
 app.get("*", function (req, res) {    
     res.sendFile(path.join(__dirname, "/public/index.html"));
 })
